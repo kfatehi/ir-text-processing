73b27490bc57971c031436f9ba11386e15fd6d45
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-01-07 11:41:09 -0800

    refactor test

--- test/src/UtilitiesTest.java
+++ test/src/UtilitiesTest.java
@@ -4,6 +4,7 @@ import junit.framework.TestCase;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by keyvan on 1/6/16.
@@ -13,17 +14,10 @@ public class UtilitiesTest extends TestCase {
 	public void testTokenizeFile() throws Exception {
 		ClassLoader classLoader = getClass().getClassLoader();
 		File file = new File(classLoader.getResource("test/fixtures/a.txt").getFile());
-		ArrayList<String> res = Utilities.tokenizeFile(file);
-		ArrayList<String> expected = new ArrayList<>();
-		expected.add("an");
-		expected.add("input");
-		expected.add("string");
-		expected.add("this");
-		expected.add("is");
-		expected.add("or");
-		expected.add("is");
-		expected.add("it");
-		assertEquals(res, expected);
+		String[] parts = "an input string this is or is it".split(" ");
+		ArrayList<String> expected = new ArrayList<>(Arrays.asList(parts));
+		ArrayList<String> actual = Utilities.tokenizeFile(file);
+		assertEquals(actual, expected);
 	}
 
 	public void testPrintFrequencies() throws Exception {
