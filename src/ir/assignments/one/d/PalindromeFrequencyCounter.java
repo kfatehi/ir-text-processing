b0b848e6385bb0a7f03d8f2d2a536237322fa737
Keyvan Fatehi <keyvanfatehi@gmail.com>
2016-01-10 11:48:06 -0800

    almost

--- src/ir/assignments/one/d/PalindromeFrequencyCounter.java
+++ src/ir/assignments/one/d/PalindromeFrequencyCounter.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Collections;
 
+import java.util.Arrays;
+
 public class PalindromeFrequencyCounter {
 	/**
 	 * This class should not be instantiated.
@@ -48,12 +50,13 @@ public class PalindromeFrequencyCounter {
 		if (words != null) {
 			HashMap<String,Frequency> map = new HashMap<>();
 
-			// we are looking ahead 1, so we stop short of the last element
-			for (int i=0; i < words.size() - 1; i++) {
-				String twoGram = words.get(i)+" "+words.get(i+1);
-			    Frequency freq = map.getOrDefault(twoGram, new Frequency(twoGram));
-				freq.incrementFrequency();
-				map.put(twoGram, freq);
+			for (int i=0; i < words.size(); i++) {
+				final String palindrome = findPalindrome(i, words);
+				if (palindrome != null)  {
+					Frequency freq = map.getOrDefault(palindrome, new Frequency(palindrome));
+					freq.incrementFrequency();
+					map.put(palindrome, freq);
+				}
 			}
 
 			ArrayList<Frequency> freqs = new ArrayList<>(map.values());
@@ -63,7 +66,63 @@ public class PalindromeFrequencyCounter {
 			return new ArrayList<>();
 		}
 	}
-	
+
+	/**
+	 * Scans the word list with respect to the index to find and return the palindrome at that point, if it exists.
+	 *
+	 * @param i the index up to which to scan
+	 * @param words the list of words to search from
+	 * @return the palindrome, if found, otherwise null
+	 */
+	private static String findPalindrome(final int i, final ArrayList<String> words) {
+		for (int k=0; k<i; k++) {
+			String possiblePalindrome = isPalindrome(words.subList(k, i));
+			if (possiblePalindrome != null)
+				return possiblePalindrome;
+		}
+		return null;
+	}
+
+	/**
+	 * Determines if a list of strings is a palindrome.
+	 *
+	 * @param strings a list of strings
+	 * @return the string if it's a palindrome, otherwise null
+	 */
+	private static String isPalindrome(List<String> strings) {
+		String str = "";
+		for (String substr : strings) {
+			if (str.length() > 0)
+				str+= " ";
+			str += substr;
+		}
+		return isPalindrome(str);
+	}
+
+	/**
+	 * Determines if a string is a palindrome.
+	 *
+	 * @param string Any string to test
+	 * @return the string if it's a palindrome, otherwise null
+	 */
+	private static String isPalindrome(String string) {
+		String normal = string.replaceAll(" ", "");
+		if (reverseString(normal).equals(normal))
+			return string;
+		else
+			return null;
+	}
+
+	/**
+	 * Produces a reversed version of the input string
+	 *
+	 * @param string the string you want to reverse
+	 * @return the reversed version of the input string
+	 */
+	private static String reverseString(String string) {
+		return new StringBuilder(string).reverse().toString();
+	}
+
 	/**
 	 * Runs the 2-gram counter. The input should be the path to a text file.
 	 * 
--- test/src/PalindromeFrequencyCounterTest.java
+++ test/src/PalindromeFrequencyCounterTest.java
@@ -45,6 +45,7 @@ public class PalindromeFrequencyCounterTest extends TestCase {
 		final ArrayList<String> words = splitOnSpaces(str);
 		final List<Frequency> list = PalindromeFrequencyCounter.computePalindromeFrequencies(words);
 		final ArrayList<Frequency> actual = new ArrayList<>(list);
+		System.out.println(actual);
 		assertEquals(3, actual.size());
 
 		assertEquals("abba", actual.get(0).getText());
